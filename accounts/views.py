1b57c00465b6c424e573b787de08958ffd8a7cac
hyunkyeng <hyunk4593@gmail.com>
2019-04-09 05:50:04 +0000

    07_make auth_form.html

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, Pass
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from .forms import UserCustomChangeForm
 
 # Create your views here.
@@ -20,7 +21,7 @@ def signup(request):
     else:
         form = UserCreationForm()
     context = {'form': form}
-    return render(request, 'accounts/signup.html', context)
+    return render(request, 'accounts/auth_form.html', context)
     
 def login(request):
     if request.user.is_authenticated:           # 로그인상태에서 로그인하면 안되므로 필요
@@ -30,10 +31,13 @@ def login(request):
          form = AuthenticationForm(request, request.POST)
          if form.is_valid():
              auth_login(request, form.get_user())
-             return redirect('boards:index')
+             return redirect(request.POST.get('next') or 'boards:index')
     else:
         form = AuthenticationForm()
-    context = {'form': form}
+    context = {
+        'form': form,
+        'next': request.GET.get('next', '')
+    }
     return render(request, 'accounts/login.html', context)
     
 def logout(request):
@@ -58,7 +62,7 @@ def edit(request):
     else:
         form = UserCustomChangeForm(instance=request.user)
     context = {'form': form,}
-    return render(request, 'accounts/edit.html', context)
+    return render(request, 'accounts/auth_form.html', context)
     
     
 def change_password(request):
@@ -71,4 +75,4 @@ def change_password(request):
     else:
         form = PasswordChangeForm(request.user)
     context = {'form': form,}
-    return render(request, 'accounts/change_password.html', context)
+    return render(request, 'accounts/auth_form.html', context)
