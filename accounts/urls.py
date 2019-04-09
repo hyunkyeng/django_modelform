548bc784bda8d57d75f8117ab5ea5a6acc2c9562
hyunkyeng <hyunk4593@gmail.com>
2019-04-09 01:42:18 +0000

    05_Authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ app_name = 'accounts'
 
 
 urlpatterns = [
+    path('edit/', views.edit, name='edit'),
+    path('delete/', views.delete, name='delete'),
     path('logout/', views.logout, name='logout'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
--- accounts/views.py
+++ accounts/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from .forms import UserCustomChangeForm
 
 # Create your views here.
 
 def signup(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated:           # 로그인 상태인지 확인. 로그인상태하면 인덱스 페이지로 보낸다.
         return redirect('boards:index')
         
     if request.method == 'POST':                    # create 
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            auth_login(request, user) 
+            user = form.save()                      # 1. 
+            auth_login(request, user)               # 2. 회원가입하자마자 로그인될 수 있도록
             return redirect('boards:index')
     else:
         form = UserCreationForm()
@@ -21,7 +22,7 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
     
 def login(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated:           # 로그인상태에서 로그인하면 안되므로 필요
         return redirect('boards:index')
     
     if request.method == 'POST':
@@ -37,3 +38,24 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('boards:index')
+    
+    
+# 회원 탈퇴
+def delete(request):        
+    user = request.user
+    if request.method == 'POST':
+        user.delete()
+    return redirect('boards:index')
+    
+def edit(request):
+    if request.method == 'POST':
+        # 수정 로직 진행
+        form = UserCustomChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('boards:index')
+    else:
+        form = UserCustomChangeForm(instance=request.user)
+    context = {'form': form,}
+    return render(request, 'accounts/edit.html', context)
+    
